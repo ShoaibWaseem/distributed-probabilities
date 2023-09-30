718f7cd058379b0efe7eec92fdf5c557810f270e
Shoaib Waseem <shoaibwaseem@Saimas-MacBook-Pro.local>
2023-09-30 16:41:02 +0100

    tidyup

--- src/main/java/com/swaseem/probability/distribution/Application.java
+++ src/main/java/com/swaseem/probability/distribution/Application.java
@@ -7,25 +7,36 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
-public class RandomGenSolution {
+public class Application {
     public static void main(String[] args) {
 
-        RandomGenSolution.arraySolution();
+        Application.arraySolution();
 
-        // Different solution but differs from spec provided
-         RandomGenSolution.treeMapSolution();
     }
 
+    /**
+     * This entrypoint to the application uses a solution with Arrays. With the provided arguments of numbers, its
+     * probabilities and a random generator, it builds a {@link RandomGen} which can then be used to loop over the next
+     * configured iterations and generate a map of results which is printed to console.
+     */
     public static void arraySolution() {
+
+        // Definte number of iterations
+        final int iterations = Integer.MAX_VALUE / 2;
+
+        // Initialise Arguments
         final Random random = new Random();
         final int[] randomNums = new int[]{5,4,3,2,1};
         final float[] randomProbabilities = new float[]{0.35f, 0.25f, 0.15f, 0.12f, 0.13f};
 
+        // Create new random generator
         final RandomGen randomGen = new RandomGen(randomNums, randomProbabilities, random);
 
+
+        // To print results, create a map
         final Map<Integer, Integer> results = new HashMap<>();
 
-        for(int i = 0; i < Integer.MAX_VALUE / 2; i++) {
+        for(int i = 0; i < iterations; i++) {
             int result = randomGen.nextNum();
 
             if (results.containsKey(result)) {
@@ -38,16 +49,26 @@ public class RandomGenSolution {
 
     }
 
+    /**
+     * Another entrypoint into the application except using {@link java.util.TreeMap} as an object to loop over.
+     * This entrypoint also allows for provided probabilities to have a sum greater than 1.
+     */
     public static void treeMapSolution() {
+        // Definte number of iterations
+        final int iterations = Integer.MAX_VALUE / 2;
+
+        // initialise Arguments
         final Random random = new Random();
         final int[] randomNums = new int[]{5,4,3,2,1};
         final float[] randomProbabilities = new float[]{0.5f, 0.4f, 0.3f, 0.2f, 0.1f};
 
+        // Create new random generator
         final TreeMapRandomGen randomGen = new TreeMapRandomGen(randomNums, randomProbabilities, random);
 
+        // To print results neatly, create a map to store results
         final Map<Integer, Integer> results = new HashMap<>();
 
-        for(int i = 0; i < Integer.MAX_VALUE / 2; i++) {
+        for(int i = 0; i < iterations; i++) {
             int result = randomGen.nextNum();
 
             if (results.containsKey(result)) {
